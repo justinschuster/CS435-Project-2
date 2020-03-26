ffd1ace25546774fb0c1b6f930c7f4403546ed66
justinschuster <schujustin@gmail.com>
2020-03-26 14:25:36 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+class GraphNode:
+    def __init__(self, data: int):
+        self.data = data
+        self.neighbors = list()
+
+class Graph:
+    def __init__(self):
+        self.nodes = list()
+
+    def addNode(nodeVal: GraphNode):
+        return
+
+def main():
+    print("Hello World")
+    return
+
+if __name__=="__main__":
+    main()
