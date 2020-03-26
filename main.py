cc6bf0a28e4b34ab22829f2d0338f3ab74553657
justinschuster <schujustin@gmail.com>
2020-03-26 14:59:41 -0400

    Now removes edges from graph

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# TODO
+# Maybe create mouse class and store directed edges there
+# directed edges can be the path the mouse takes
+class DirectedEdge:
+    def __init__(self, source, dest) -> None:
+        self.source = source
+        self.dest = dest
+ 
 class UndirectedEdge:
     def __init__(self, first, second) -> None:
         self.first = first
@@ -25,8 +33,12 @@ class Graph:
         self.un_edges.append(newEdge)
 
     # removes an undirected edge between first and second (and vice versa)
-    def removeUndirectedEdge(first: GraphNode, second: GraphNode) -> None:
-        return
+    def removeUndirectedEdge(self, first: GraphNode, second: GraphNode) -> None:
+        for edge in self.un_edges:
+            if (edge.first == first and edge.second == second):
+                 self.un_edges.remove(edge)
+            elif (edge.first == second and edge.second == first):
+                self.un_edges.remove(edge)
     
     # returns a set of all Nodes in the graph
     def getAllNodes(self):
@@ -42,7 +54,7 @@ def main() -> None:
         #print(node.data)
 
     maze.addUndirectedEdge(first, second)
-
+    maze.removeUndirectedEdge(first, second)
     for edge in maze.un_edges:
         print(edge.first.data)
 
