e2717c6d39235854e30823b768003c920a209b42
justinschuster <schujustin@gmail.com>
2020-04-03 15:31:32 -0400

    created the DirectedGraph class

---
+++ DirectedGraph.py
@@ -0,0 +1,19 @@
+from Graph import Graph 
+
+class DirectedGraph(Graph):
+
+    # adds a new node to the graph
+    def addNode(nodeVal) -> None:
+        pass
+
+    # adds a directed edge between first and second
+    def addDirectedEdge(first, second) -> None:
+        pass
+
+    # removes directed edge between two nodes
+    def removeDirectedEdge(first, second) -> None:
+        pass
+
+    # returns a set of all Nodes in the graph 
+    def getAllNodes():
+        pass 
