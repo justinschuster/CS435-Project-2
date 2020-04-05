9ce1a7074741f07ab48fae0c0efdf8a3dec45eab
justinschuster <schujustin@gmail.com>
2020-04-04 21:29:14 -0400

    Kahns method sorta works. If the nodes with in degree == 0 aren't connected to everything it wont print the unconnected nodes. Need to fix this

--- Part 2/sorting.py
+++ Part 2/sorting.py
@@ -5,10 +5,9 @@
 from DirectedGraph import DirectedGraph
 
 class TopSort:
-
     # does a valid topological sort using Kahn's algorithm 
     @staticmethod
-    def Kahns(graph: DirectedGraph) -> list():
+    def Kahns(graph: DirectedGraph):
         # let M be a dict mapping each node to its total dependencies graph.adjList is dependency dict
         # let A be an array of nodes to be outputted
         output = []
@@ -17,20 +16,38 @@ class TopSort:
         queue = []
 
         # add all elements of M whose values are 0 to Q
-        for node in graph.adjList.keys():
-            if len(graph.adjList[node]) is 0:
+        for node in graph.getAllNodes():
+            print("Node: %d, in degree: %d"% (node.value, node.inDegree))
+            if node.inDegree is 0:
                 queue.append(node)
 
         # while Q is not empty
         while len(queue) is not 0:
             # N <-- first element in Q
-            # add N to A
-            # decrement the in-degrees of all dependents of N
-            # decrement the in-degree of N to -1 (to avoid adding it back to Q)
-            # for each neighbor <-- N's neighbors
-                # if neighbor has in-degree 0
-                    # add neighbor to Q
-            # remove N from Q
-        # output all nodes in Q in order 
+            currNode = queue.pop(0)
 
-        pass 
+            # add N to A
+            output.append(currNode)
+
+            # decrement the in-degrees of all dependents of N
+            for neighbor in graph.adjList[currNode]:
+                neighbor.inDegree = neighbor.inDegree - 1
+
+                if neighbor.inDegree is 0:
+                    queue.append(neighbor)
+
+            # decrement the in-degree of N to -1 (to avoid adding it back to Q)
+            currNode.inDegree = -1
+
+            # for each neighbor <-- N's neighbors
+            #for neighbor in graph.adjList[currNode]:
+                # if neighbor has in-degree 0
+                #if neighbor.inDegree is 0:
+                    # add neighbor to Q
+                    #queue.append(neighbor)
+
+            # remove N from Q
+            #queue.remove(currNode)
+
+        # output all nodes in Q in order 
+        return output
